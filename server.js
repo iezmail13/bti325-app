2de23261b19113dd22ad0a6d23ad36de5bd1327c
Ismail Mohamed <imohamed24@myseneca.ca>
2022-10-11 21:11:07 -0400

    initial commit

--- server.js
+++ server.js
@@ -7,67 +7,85 @@ other source
 *
 * Name: Ismail Mohamed Student ID: 107859217 Date: 10/09/22
 *
-* Your app’s URL (from Cyclic) : ______________________________________________
+* Your app’s URL (from Cyclic) : https://zany-gold-dalmatian-hat.cyclic.app/
 *
 *************************************************************************/ 
 
-var data_service = require ('data-service.js');
-
-import * as data_service from '/data-service.js';
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "/data/employees.json"));})
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "/data/departments.json"));})
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "/data/managers.json"));})
-        
-
 // http://localhost:8080
 var express = require("express"); // Include express.js module
+const { Server } = require("http");
 var app = express();
+var data_Server = require('./data-service')
+var path = require("path"); // include moduel path to use dirname, and function path.join()
 
-var path = require("path"); // include module path to use __dirname, and function path.join()
-
-var HTTP_PORT = process.env.PORT || 8080;  // || : or
+var HTTP_PORT = process.env.PORT || 8080;  //  : or
 
 // call this function after the http server starts listening for requests
 function onHttpStart(){
     console.log("Express http server listening on: " + HTTP_PORT);
-}
+};
 
 app.use(express.static('public'));
 
-// setup a 'route' to listen on the default url path (http://localhost)
+// setup a 'route' to listen on the default url path (http://localhost/)
 app.get("/", function(req, res){
-    let resText = "Hello World. <br> This is my homepage. <br> Enjoy... <br>";
-    resText += "<a href = './about/html'> Go to About page </a> <br>";
-    resText += "<a href = 'home.html'> Go to Home File page </a> <br>"; 
-    resText += " Happy Coding! "; 
-   
+    res.sendFile(path.join(__dirname, "/views/home.html"));
 });
 
- // setup another route to listen on /about
 app.get("/about", function (req, res){
-    var resText = "<h3> This is from route /About. </h3> <br>";
-    resText += "<a href='/about'> About </a>";
     res.sendFile(path.join(__dirname, "/views/about.html"));
 });
-
-/* app.get("/contact", function (req, res){
-    var resText = "<h3> This is from route /About. </h3> <br>";
-    resText += "<a href='/'> Home </a>";
-    res.send(resText);
-});
-
-*/
 
 // setup another route to listen on /aboutFile
-app.get("/aboutfile", function(req, res){
+app.get("/aboutFile", function(req, res){
     res.sendFile(path.join(__dirname, "/views/about.html"));
 });
 
+app.get('/employees', function (req, res) {
+  data_Server
+    .getAllEmployees()
+    .then((data) => {
+  res.json(data);
+})
+    .catch((err) => {
+  res.json({ message: err });
+});
+  });
+
+  app.get('/managers', function (req, res) {
+    data_Server
+    .getManagers()
+    .then((data) => {
+  res.json(data);
+})
+    .catch((err) => {
+    res.json({ message: err });
+});
+  });
+
+  app.get('/departments', function (req, res) {
+    data_Server
+    .getDepartments()
+    .then((data) => {
+  res.json(data);
+})
+    .catch((err) => {
+    res.json({ message: err });
+});
+  });
+  
+  app.get('*', function (req, res) {
+  res.send('Error(404): File Not Found');
+  });
+
 //setup http server to listen on HTTP_PORT
+data_Server
+.initialize()
+.then(() => {
+
 app.listen(HTTP_PORT, onHttpStart);
+})
+
+.catch(function (err) {
+console.log(err);
+});
